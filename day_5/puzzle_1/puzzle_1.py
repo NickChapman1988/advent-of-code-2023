296d99d733bdaee0dd7b5917b281957a9c66a266
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-12-16 21:19:06 +0000

    Reset Day 5 solution

--- day_5/puzzle_1/puzzle_1.py
+++ day_5/puzzle_1/puzzle_1.py
@@ -3,27 +3,4 @@ Day 5 Puzzle 1 Solution
 
 """
 
-file = open('day_5/puzzle_1/seeds.txt', 'r', encoding="utf-8")
-lines = file.readlines()
 
-seeds = lines.split("\n\n")[0].split(" ");
-
-
-seedToSoil = lines.split("\n\n")[1].split("\n");
-
-
-soilToFertilizer = lines.split("\n\n")[2].split("\n");
-
-
-fertilizerToWater = lines.split("\n\n")[3].split("\n");
-
-waterToLight = lines.split("\n\n")[4].split("\n");
-
-
-lightToTemperature = lines.split("\n\n")[5].split("\n");
-
-  
-temperatureToHumidity = lines.split("\n\n")[6].split("\n");
-
-
-humidityToLocation = lines.split("\n\n")[7].split("\n");
